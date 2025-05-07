7d032b2ae7fec4e577c8d171495d4821eb38ad7c
Martin-NT <martinnavarro1503@gmail.com>
2025-05-07 12:09:49 -0300

    Implementación Repository

--- TP4/src/main/java/com/example/TP4/repository/UsuarioRepository.java
+++ TP4/src/main/java/com/example/TP4/repository/UsuarioRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public interface UsuarioRepository {
     Usuario save(Usuario usuario);
     Optional<Usuario> findById(Long id);
+    Optional<Usuario> findByNombreCompleto(String nombre, String apellido);
     List<Usuario> findAll();
     void deleteById(Long id);
     boolean existsById(Long id);
